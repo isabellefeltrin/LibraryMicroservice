6a001b8347e2ec321e6cc25c0c1db5debc309e0b
isabellefeltrin <isalufeltrin@gmail.com>
2025-11-25 22:45:01 -0300

    ajustes

--- DTO/Books/BookResponseDTO.cs
+++ DTO/Books/BookResponseDTO.cs
@@ -3,9 +3,9 @@
     public class BookResponseDTO
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Category { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
         public int Quantity { get; set; }
     }
 }
